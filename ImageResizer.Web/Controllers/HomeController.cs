f75b3eff8db710d92b01cdf9a98959dfc9efa9d3
karimabd84 <abdelkarim.esti@gmail.com>
2017-09-25 11:58:16 +0200

    ImageResizer added

---
+++ ImageResizer.Web/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageResizer.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ResizeImage()
+        {
+            ViewBag.Message = "Upload image";
+
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult ResizeImage(HttpPostedFileBase imageToUpload)
+        {
+            if(imageToUpload != null)
+            {
+                //save image
+                string filename = Path.GetFileNameWithoutExtension(imageToUpload.FileName);
+                filename = DateTime.Now.ToString("yyyyMMddhhmmss") + filename;
+                using (var image = Image.FromFile(imageToUpload.FileName))
+                using (var newImage = ImageCreator.ScaleImage(image, 800, 600))
+                {
+                    newImage.Save(Server.MapPath("../Images/" + filename + ".jpg"), ImageFormat.Jpeg);
+                    //newImage.Save(@"c:\test.png", ImageFormat.Png);
+                }
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    imageToUpload.InputStream.CopyTo(ms);
+                    //return File(ImageCreator.ResizeImage(ms.GetBuffer(), 800, 600),"image /jpeg");
+                    return File(ImageCreator.ScaleImage(ms.GetBuffer(), 800, 600,ImageFormat.Jpeg), "image/jpeg");
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ ImageResizer.Web/ImageCreator.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageResizer.Web
+{
+    public class ImageCreator
+    {
+
+        public static byte[] ScaleImage(byte[] image, int width,int height, ImageFormat format)
+        {
+            Image result = null;
+            try
+            {
+                MemoryStream imageStream = new MemoryStream(image);
+                result = Image.FromStream(imageStream);
+            }
+            catch
+            {
+                //result = GetNoImage();
+            }
+
+            if (result == null) return image;
+
+            var newImage = ScaleImage(result, width, height);
+            using (MemoryStream ms = new MemoryStream())
+            {
+                newImage.Save(ms, format);
+                return ms.GetBuffer();
+            }
+        }
+
+        public static Image ScaleImage(Image image, int width, int height)
+        {
+            var ratioX = (double)width / image.Width;
+            var ratioY = (double)height / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+            var newImage = new Bitmap(newWidth, newHeight);
+            using (var graphics = Graphics.FromImage(newImage))
+            {
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+            }
+            return newImage;
+        }
+
+        public static byte[] ResizeImage(byte[] image, int width, int height)
+        {
+            Image result = null;
+            try
+            {
+                MemoryStream imageStream = new MemoryStream(image);
+                result = Image.FromStream(imageStream);
+            }
+            catch
+            {
+                //result = GetNoImage();
+            }
+
+            int newWidth = result.Width;
+            int newHeight = result.Height;
+            if (width == 0) width = newWidth;
+            if (height == 0) height = newHeight;
+
+            if (newWidth > width || newHeight > width)
+            {
+                float ratioWidth = (float)width / result.Width;
+                float ratioHeight = (float)height / result.Height;
+                if (ratioWidth > ratioHeight)
+                {
+                    newWidth = width;
+                    newHeight = Convert.ToInt32(Math.Floor(newHeight * ratioWidth));
+                }
+                else
+                {
+                    newHeight = width;
+                    newWidth = Convert.ToInt32(Math.Floor(newWidth * ratioHeight));
+                }
+            }
+
+            using (var bmp = new Bitmap(width, newHeight))
+            {
+                using (Graphics graphics = Graphics.FromImage(bmp))
+                {
+                    graphics.FillRectangle(Brushes.White, 0, 0, width, newHeight);
+                    graphics.DrawImage(result, (float)(width - newWidth) / 2, (float)(0), newWidth, newHeight);
+                }
+                return GetBufferFromImage(bmp, ImageFormat.Jpeg, 80);
+            }
+        }
+
+        private static byte[] GetBufferFromImage(Image image, ImageFormat format, long quality)
+        {
+            var ms = new MemoryStream();
+            if (format == ImageFormat.Jpeg)
+                image.Save(ms, GetEncoder(format), GetJpegEncoderParams(quality));
+            else
+                image.Save(ms, format);
+
+            return ms.GetBuffer();
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            var codecs = ImageCodecInfo.GetImageDecoders();
+            return codecs.Where(x => x.FormatID == format.Guid).FirstOrDefault();
+        }
+
+        private static EncoderParameters GetJpegEncoderParams(long quality)
+        {
+            var encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, quality);
+            return encoderParams;
+        }
+    }
+}
